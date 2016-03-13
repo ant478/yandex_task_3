7ec9fd61ad6abddfe35f5153e305095f7e0e8232
ant478 <ant478@gmail.com>
2016-03-14 02:13:14 +0300

    Refactored, added design, added play/pause buttons

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ function start () {
     var camera = document.querySelector('.camera'),
         video = document.querySelector('.camera__video'),
         canvas = document.querySelector('.camera__canvas'),
-        FPSBlock = document.querySelector('.fps'),
+        FPSBlock = document.querySelector('.controls__fps'),
+        resolutionBlock = document.querySelector('.controls__resolution'),
+        filters_container = document.querySelector('.controls__filter_container'),
+        filters_wrapper = document.querySelector('.filters_wrapper'),
+        select_next_filter_button = document.querySelector('.controls__filter_next'),
+        select_previous_filter_button = document.querySelector('.controls__filter_previous'),
+        buttons_container = document.querySelector('.controls__buttons'),
+        buttons_wrapper = document.querySelector('.controls__buttons_wrapper'),
+        play_button = document.querySelector('.controls__buttons_play'),
+        pause_button = document.querySelector('.controls__buttons_pause'),
         context = canvas.getContext('2d'),
-        filterName = '',
         imageData = null,
+        selectedFilter = null,
         width = 0,
         height = 0,        
         FPS = 0;
         index = 0,
         pixels = [],
-        filters = {
-            invert: invert,
-            threshold: threshold,
-            grayscale: grayscale
-        };
+        filters = [noop, invert, threshold, grayscale];
+
+    window.addEventListener("resize", resizeCanvas);
+    select_next_filter_button.addEventListener("click", selectNextFilter);
+    select_previous_filter_button.addEventListener("click", selectPreviousFilter);
+    play_button.addEventListener("click", play);
+    pause_button.addEventListener("click", pause);
+
+    window.requestAnimFrame = (function() {
+        return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
 
     function getVideoStream (callback) {
         navigator.getUserMedia = navigator.getUserMedia ||
@@ -39,6 +59,8 @@ function start () {
             console.log("getUserMedia not supported");
         }
     };
+
+    function noop () {};
 
     function invert () {
         for (index = 0; index < pixels.length; index += 4) {
@@ -64,20 +86,34 @@ function start () {
 
     function applyFilter () {
         try {
-            resizeCanvas();
-            context.drawImage(video, 0, 0, width, height);
-            imageData = context.getImageData(0, 0, width, height);
-            pixels = imageData.data;
-            filters[filterName]();
-            context.putImageData(imageData, 0, 0);
-            FPS++;
-        } finally {
+            if (video.currentTime > 0 && !video.paused && !video.ended) {
+                width = canvas.width = camera.offsetWidth;
+                height = canvas.height = camera.offsetHeight;
+                context.drawImage(video, 0, 0, width, height);
+                imageData = context.getImageData(0, 0, width, height);
+                pixels = imageData.data;
+                selectedFilter();
+                context.putImageData(imageData, 0, 0);
+                FPS++;
+            }
+        } finally {            
             window.requestAnimFrame(applyFilter);
         }
     };
 
-    function changeFilter (event) {
-        filterName = event.target.value;
+    function selectFilter (index) {
+        if (index >= 0 && index < filters.length) {
+            selectedFilter = filters[index];
+            filters_wrapper.style.left = -index * filters_container.offsetWidth + 'px';
+        }
+    };
+
+    function selectPreviousFilter () {
+        selectFilter(filters.indexOf(selectedFilter) - 1);
+    };
+
+    function selectNextFilter () {
+        selectFilter(filters.indexOf(selectedFilter) + 1);
     };
 
     function showFPS () {
@@ -85,35 +121,34 @@ function start () {
         FPS = 0;
     };
 
+    function showResolution () {
+        resolutionBlock.textContent = width + 'x' + height;
+    };
+
+    function play () {
+        video.play();
+        buttons_wrapper.style.left = '0px';
+    };
+
+    function pause () {
+        video.pause();
+        buttons_wrapper.style.left = -buttons_container.offsetWidth + 'px';
+        FPS = 0;
+    };
+
     function resizeCanvas () {
         width = canvas.width = camera.offsetWidth;
         height = canvas.height = camera.offsetHeight;
+        showResolution();
     };
 
     function init () {
-        window.requestAnimFrame = (function() {
-          return  window.requestAnimationFrame       ||
-                  window.webkitRequestAnimationFrame ||
-                  window.mozRequestAnimationFrame    ||
-                  function( callback ){
-                    window.setTimeout(callback, 1000 / 60);
-                  };
-        })();
-        var controlFilters = document.getElementsByClassName('controls__filter');
-        for (var i = 0; i < controlFilters.length; i++) {
-            controlFilters[i].addEventListener("change", changeFilter);
-            if (controlFilters[i].checked) {
-                filterName = controlFilters[i].value;
-            }
-        }
-        if (!filterName) 
-            filterName = 'grayscale';
-        width = canvas.width = camera.offsetWidth;
-        height = canvas.height = camera.offsetHeight;
-        window.addEventListener("resize", resizeCanvas);
+        selectFilter(0);
+        resizeCanvas();
+        showFPS();
         setInterval(showFPS, 1000);
         applyFilter();        
-    }
+    };
 
     getVideoStream(init);
 };
